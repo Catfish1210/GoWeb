fa7658302265d12f75724fc1c8dca59935b145dd
Catfish1210 <chris.laks@gmail.com>
2023-07-20 14:11:43 +0300

    working server-side calculator

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module webserver
 
 go 1.18
+
+require github.com/Knetic/govaluate v3.0.0+incompatible // indirect
--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-	"unicode"
+
+	"github.com/Knetic/govaluate"
 )
 
 type PageData struct {
@@ -18,8 +18,8 @@ func main() {
 			r.ParseForm()
 			result := r.Form.Get("result")
 			fmt.Println("Before Calculator Result: " + result)
-			calculateResult(result)
-			// fmt.Println("After Calculator Result: ", calculateResult(result))
+			// calculateResult(result)
+			fmt.Println("After Calculator Result: ", calculateResult(result))
 			w.WriteHeader(http.StatusOK)
 		} else if r.URL.Path == "/favicon.ico" {
 			w.Header().Set("Content-Type", "image/x-icon")
@@ -35,118 +35,24 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-func calculateResult(input string) {
-	fmt.Println(Calculate(input))
-	// var num string
-	// var numSlice []string
-	// operatorMap := map[string][]int{
-	// 	"+": {},
-	// 	"-": {},
-	// 	"÷": {},
-	// 	"%": {},
-	// 	"x": {},
-	// }
-
-	// for i, char := range input {
-	// 	fmt.Println("Iteration: ", i, "On char: ", string(char))
-
-	// 	if isOperator(char) {
-	// 		fmt.Println("-isOperator: True", string(char))
-	// 		switch string(char) {
-	// 		case "+":
-	// 			operatorMap["+"] = append(operatorMap["+"], i)
-	// 		case "-":
-	// 			operatorMap["-"] = append(operatorMap["-"], i)
-	// 		case "÷":
-	// 			operatorMap["÷"] = append(operatorMap["÷"], i)
-	// 		case "%":
-	// 			operatorMap["%"] = append(operatorMap["%"], i)
-	// 		case "x":
-	// 			operatorMap["x"] = append(operatorMap["x"], i)
-	// 		default:
-	// 			fmt.Println("Invalid operator")
-	// 		}
-
-	// 		numSlice = append(numSlice, num)
-	// 		num = ""
-	// 	} else {
-	// 		num += string(char)
-	// 		if i == len(input)-1 {
-	// 			numSlice = append(numSlice, num)
-	// 		}
-	// 	}
-	// 	fmt.Println("--Num string at end of for loop: ", num)
-	// 	fmt.Println("--NumSlice string at end of for loop: ", numSlice)
-	// }
-
-	// fmt.Println("Num slice: ", numSlice)
-	// fmt.Println("Operator map: ", operatorMap)
-
-	// Calculate(AtoiSlice(numSlice), operatorMap)
-
-}
-
-func Calculate(expression string) int {
-	var num int
-	var operator = '+'
-	var result int
-	var total int
-
-	for _, char := range expression {
-		if unicode.IsDigit(char) {
-			digit, _ := strconv.Atoi(string(char))
-			num = num*10 + digit
-		} else if isOperator(char) {
-
-			result = applyOperation(result, operator, num)
-			total += result
-			num = 0
-			operator = char
-		} else {
-			return 0
-		}
+func calculateResult(input string) int {
+	expr, err := govaluate.NewEvaluableExpression(input)
+	if err != nil {
+		return 0
 	}
-	result = applyOperation(result, operator, num)
 
-	total += result
-	return total
-}
+	result, err := expr.Evaluate(nil)
+	if err != nil {
+		return 0
+	}
 
-func applyOperation(currentResult int, operator rune, num int) int {
-	switch operator {
-	case '+':
-		return currentResult + num
-	case '-':
-		return currentResult - num
-	case 'x':
-		return currentResult * num
-	case '÷':
-		return currentResult / num
+	switch res := result.(type) {
+	case int:
+		return res
+	case float64:
+		return int(res)
 	default:
-		return num
-	}
-}
-
-func isOperator(char rune) bool {
-	opCheck := string(char)
-	if opCheck == "+" || opCheck == "-" || opCheck == "÷" || opCheck == "%" || opCheck == "x" {
-		return true
-	} else {
-		return false
-	}
-}
-
-func AtoiSlice(numSlice []string) []int {
-	var sliceInt []int
-
-	for _, number := range numSlice {
-		res := 0
-		for _, ch := range number {
-			digit := int(ch - '0')
-			res = res*10 + digit
-		}
-		sliceInt = append(sliceInt, res)
+		return 0
 	}
 
-	return sliceInt
 }
